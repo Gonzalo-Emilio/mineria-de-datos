7d77e902ea92fea28d523608d1cba8a4ef234fec
Gonzalo-Emilio <gonzooooo10@gmail.com>
2022-05-11 15:13:02 -0500

    Selección de características v2

--- Seleccion_de_caracteristicas.py
+++ Seleccion_de_caracteristicas.py
@@ -1,4 +1,6 @@
 #analisis exploratorio de datos
+from io import SEEK_CUR
+from tkinter.tix import Select
 from xml.etree.ElementInclude import include
 import streamlit as st
 import pandas as pd
@@ -8,39 +10,27 @@ import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler, MinMaxScaler  
 
-def seleccion(Data):
+def seleccion_de_caracteristicas(Data):
     st.subheader("ACP")
     st.write("**Estandarización de Datos**")
     st.write("Elección de borrado de Columnas de tipo Object")
     drop_column = borrado(Data)
+    na_cero = drop_column.fillna(0)
     if drop_column is not None:
         sel = st.selectbox('Selecciona una forma de estandarización de datos',
         ('','Estandarización', 'Normalización'))
         if sel == 'Estandarización':
             st.subheader('Estandarización')
             Estandarizar = StandardScaler()
-            MEstantadar = Estandarizar.fit_transform(drop_column)
-            st.dataframe(MEstantadar)
-            pca = PCA(0.85)     # pca=PCA(n_components=None), pca=PCA(.85)
-            pca.fit(MEstantadar)          # Se obtiene los componentes
-            st.write("**Matriz Componentes**")
-            st.dataframe(pca.components_)
-            st.write('**Varianza**')
-            Varianza = pca.explained_variance_ratio_
-            st.write('Proporción de varianza:',Varianza.tolist())
+            seleccion(Estandarizar, na_cero)
 
         elif sel == 'Normalización':
             st.subheader('Normalización')
             Normalizar = MinMaxScaler() 
-            MNormalizar = Normalizar.fit_transform(drop_column)
-            st.dataframe(MNormalizar) 
-            pca = PCA(0.85)     # pca=PCA(n_components=None), pca=PCA(.85)
-            pca.fit(MNormalizar)          # Se obtiene los componentes
-            st.write('**Matriz Componentes**')
-            st.write(pca.components_)
-            st.write('**Varianza**')
-            Varianza = pca.explained_variance_ratio_
-            st.write('Proporción de varianza:', Varianza.tolist())
+            seleccion(Normalizar, na_cero)
+            
+            
+
 
 def borrado(Data):
     options = st.multiselect(
@@ -61,5 +51,37 @@ def borrado(Data):
 
 
 
-#def seleccion(Estandar):
+def seleccion(Estandarizar, na_cero):
+    sum_varianza = 0 
+    MEstandar = Estandarizar.fit_transform(na_cero)
+    st.dataframe(MEstandar)
+    pca = PCA(0.85)     # pca=PCA(n_components=None), pca=PCA(.85)
+    pca.fit(MEstandar)          # Se obtiene los componentes
+    st.write("**Matriz Componentes**")
+    st.dataframe(pca.components_)
+    st.write('**Varianza**')
+    Varianza = pca.explained_variance_ratio_
+    varianza2 = Varianza.tolist()
+    st.write('Proporción de varianza:', varianza2)
+    sum_varianza=st.number_input("Selecciona el numero de varianzas a sumar")
+    if sum_varianza >= len(varianza2):
+            st.write("Digita un valor en rango")
+    else:
+        suma = sum(Varianza[0:int(sum_varianza)+1])
+        if suma >= 0.75 and suma <= 0.90:
+            st.write("Suma de varianza:", suma, "Aceptable")
+        else:
+            st.write("Suma de varianza:", suma, "no es aceptable")
     
+    st.write("**Gráfica de Varianza**")
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('Número de componentes')
+    plt.ylabel('Varianza acumulada')
+    plt.grid()
+    plt.show()
+    st.pyplot()
+
+    st.write("**Proporción de Relevancia**")
+    st.dataframe(pd.DataFrame(pca.components_, columns=na_cero.columns))
+    st.write("**Identificación de Relevancia de Variables**")
+    st.write()
--- funciones.py
+++ funciones.py
@@ -5,7 +5,7 @@ import numpy as np                # Para crear vectores y matrices n dimensional
 import matplotlib.pyplot as plt         # Para la generación de gráficas a partir de los datos
 import seaborn as sns  
 from EDA import EDA        
-from Seleccion_de_caracteristicas import seleccion          # Para la visualización de datos basado en matplotlib               
+from Seleccion_de_caracteristicas import  seleccion_de_caracteristicas          # Para la visualización de datos basado en matplotlib               
 
 #simulacion swtich case
 def Switch(Election,Data):
@@ -14,7 +14,7 @@ def Switch(Election,Data):
     elif Election == 'Análisis Exploratorio de Datos':
         EDA(Data)
     elif Election == 'Selección de características':
-        seleccion(Data)
+        seleccion_de_caracteristicas(Data)
     elif Election == 'Métricas de Distancia':
         st.text('holaaa soy metri xd') 
     elif Election == 'Clustering':
